16ca275030f1cb82cf0a30e657ec755b6a3d0bfb
Mauricio Magalhaes <contato@mauriciommagalhaes.com.br>
2022-04-24 22:40:04 -0300

    .

--- 02_formularios/forms/src/App.js
+++ 02_formularios/forms/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h2>Forms</h2>
-      <MyForm />
+      <MyForm user={{ name: 'Josias', email: 'josias@gmail.com', bio: 'sou programador', role: 'admin' }} />
     </div>
   );
 }
--- 02_formularios/forms/src/components/MyForm.js
+++ 02_formularios/forms/src/components/MyForm.js
@@ -1,28 +1,73 @@
-import { useState } from 'react'
-import './MyForm.css'
-const MyForm = () => {
-    {/* 3- Gerenciamento de dados */ }
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
+import { useState } from "react";
+import "./MyForm.css";
 
+const MyForm = ({ user }) => {
+  {
+    /* 3- Gerenciamento de dados */
+  }
+  const [name, setName] = useState(user ? user.name : "");
+  const [email, setEmail] = useState(user ? user.email : "");
 
-    return (
+  const handleChange = (e) => setName(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, bio, role);
+    setName("");
+    setEmail("");
+    setBio("");
+  };
+
+  const [bio, setBio] = useState(user ? user.bio : "");
+  const [role, setRole] = useState(user ? user.role : "");
+
+  return (
+    <div>
+      {/*Criacao de formulario*/}
+      <form onSubmit={handleSubmit}>
         <div>
-            {/*Criacao de formulario*/}
-            <form>
-                <div>
-                    <label htmlFor='name'>Nome:</label>
-                    <input type="text" name="nome" placeholder="Disgite seu nome" onChange={handleChange} />
-                </div>
-                {/* laBEL ENVOLVENDO INPUT */}
-                <label>
-                    <span>E-mail</span>
-                    <input type="email" name="email" placeholder="Digite o seu e-mail" />
-                </label>
-                <input type="submit" value="Enviar" />
-            </form>
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            name="nome"
+            placeholder="Digite seu nome"
+            value={name}
+            onChange={handleChange}
+          />
         </div>
-    )
-}
+        {/* laBEL ENVOLVENDO INPUT */}
+        <label>
+          <span>E-mail</span>
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite o seu e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Bio:</span>
+          <textarea
+            name="bio"
+            placeholder="Descrição do usuário"
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+          ></textarea>
+        </label>
+        <label>
+          <span>Função do sistema:</span>
+          <select name="role" onChange={e => setRole(e.target.value)} value={role}>
+            <option value="user">Usuário</option>
+            <option value="editor">Editor</option>
+            <option value="admin">Administrador</option>
+          </select>
+        </label>
 
-export default MyForm
+        <input type="submit" value="Enviar" />
+      </form>
+    </div>
+  );
+};
+
+export default MyForm;
