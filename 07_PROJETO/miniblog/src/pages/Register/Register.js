33c12b7004122e270f7648dc7858525394ffd530
Mauricio Magalhaes <contato@mauriciommagalhaes.com.br>
2022-05-01 19:22:26 -0300

    .

---
+++ 07_PROJETO/miniblog/src/pages/Register/Register.js
@@ -0,0 +1,13 @@
+import styles from "./Register.module.css";
+
+import { useState, useEffect } from "react";
+
+const Register = () => {
+    return (
+        <div>
+            <h1>Cadastre-se para postar</h1>
+        </div>
+    );
+};
+
+export default Register;
