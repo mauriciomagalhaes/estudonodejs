55662cd67bef9f71303a60c6ac86a272c89ba5e3
Mauricio Magalhaes <contato@mauriciomagalhaes.com.br>
2022-05-26 16:42:31 -0300

    .

--- 07_PROJETO/miniblog/src/components/PostDetail.js
+++ 07_PROJETO/miniblog/src/components/PostDetail.js
@@ -2,10 +2,11 @@ import styles from "./PostDetail.module.css";
 
 const PostDetail = ({ post }) => {
     return (
-        <>
+        <div>
             <img src={post.image} alt={post.title} />
             <h2>{post.title}</h2>
-        </>
+            <p>Por: {post.createdBy}</p>
+        </div>
     );
 };
 
