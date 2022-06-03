e91bb63f33b05c2497435e44a022410969f640c4
Mauricio Magalhaes <contato@mauriciomagalhaes.com.br>
2022-06-03 16:24:16 -0300

    .

--- 07_PROJETO/miniblog/src/hooks/useFetchDocuments.js
+++ 07_PROJETO/miniblog/src/hooks/useFetchDocuments.js
@@ -57,7 +57,7 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
             }
         }
         loadData();
-    }, [docCollection, search, uid, cancelled]);
+    }, [docCollection, documents, search, uid, cancelled]);
     //console.log(documents);
     useEffect(() => {
         return () => setCancelled(true);
